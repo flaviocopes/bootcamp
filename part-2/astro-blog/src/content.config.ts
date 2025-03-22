daea3d5fe23f144935b96ff6ad60732c231c99f1
Flavio Copes <flavio@flaviocopes.com>
2025-03-22 10:52:26 +0100

    Avoid confusion with prior content collections lesson, use `src/posts` instead of `src/content/posts` to host markdown files

--- part-2/astro-blog/src/content.config.ts
+++ part-2/astro-blog/src/content.config.ts
@@ -2,7 +2,7 @@ import { z, defineCollection } from 'astro:content'
 import { glob } from 'astro/loaders'
 
 const posts = defineCollection({
-  loader: glob({ pattern: '*.md', base: './src/content/posts' }),
+  loader: glob({ pattern: '*.md', base: './src/posts' }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
